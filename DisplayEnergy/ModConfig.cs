e01a6eeceacb094309087edf87f530f1b38310d4
Dany Marcoux <git@dmarcoux.com>
2024-07-22 22:41:33 +0200

    Allow the energy text's position to be configurable in-game
    Closes #1

---
+++ DisplayEnergy/ModConfig.cs
@@ -0,0 +1,8 @@
+using Microsoft.Xna.Framework;
+
+namespace dmarcoux.Stardew.DisplayEnergy;
+
+public sealed class ModConfig
+{
+    public Vector2 EnergyTextOffset { get; set; } = new(215, 60);
+}
--- DisplayEnergy/ModEntry.cs
+++ DisplayEnergy/ModEntry.cs
@@ -12,12 +12,20 @@ namespace dmarcoux.Stardew.DisplayEnergy
     public class ModEntry : Mod
     {
         /*********
+        ** Properties
+        *********/
+        /// <summary>The mod configuration from the player.</summary>
+        private ModConfig Config;
+
+        /*********
         ** Public methods
         *********/
         /// <summary>The mod entry point, called after the mod is first loaded.</summary>
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
+            this.Config = this.Helper.ReadConfig<ModConfig>();
+            helper.Events.GameLoop.GameLaunched += this.RegisterForGenericModConfigMenu;
             helper.Events.Display.RenderingHud += this.DisplayStamina;
         }
 
@@ -30,12 +38,68 @@ namespace dmarcoux.Stardew.DisplayEnergy
         /// <param name="e">The event data.</param>
         private void DisplayStamina(object sender, RenderingHudEventArgs e)
         {
-            int textRightPosition = (Game1.showingHealth) ? 265 : 215;
+            Vector2 energyTextOffset = this.Config.EnergyTextOffset;
+            if (Game1.showingHealth)
+            {
+                float x = energyTextOffset.X;
+                energyTextOffset = energyTextOffset with { X = x + 50 };
+            }
 
             if (!Game1.eventUp)
             {
-                Game1.spriteBatch.DrawString(Game1.dialogueFont, $"{(int)Game1.player.Stamina}/{Game1.player.maxStamina}", new Vector2(Game1.graphics.GraphicsDevice.Viewport.GetTitleSafeArea().Right - textRightPosition, Game1.graphics.GraphicsDevice.Viewport.GetTitleSafeArea().Bottom - 60), Color.White);
+                Game1.spriteBatch.DrawString(Game1.dialogueFont, $"{(int)Game1.player.Stamina}/{Game1.player.maxStamina}", new Vector2(Game1.graphics.GraphicsDevice.Viewport.GetTitleSafeArea().Right - energyTextOffset.X, Game1.graphics.GraphicsDevice.Viewport.GetTitleSafeArea().Bottom - energyTextOffset.Y), Color.White);
             }
+        }
+
+        /// <summary>
+        /// Raised after the game is launched, right before the first update tick.
+        /// This happens once per game session (unrelated to loading saves).
+        /// All mods are loaded and initialised at this point, so this is a good time to set up mod integrations.
+        /// </summary>
+        /// <param name="sender">The event sender</param>
+        /// <param name="e">The event data</param>
+        private void RegisterForGenericModConfigMenu(object sender, GameLaunchedEventArgs e)
+        {
+            // Get Generic Mod Config Menu's API (if it's installed)
+            var configMenu = this.Helper.ModRegistry.GetApi<IGenericModConfigMenuApi>("spacechase0.GenericModConfigMenu");
+            if (configMenu is null)
+            {
+                return;
+            }
+
+            // Register this mod
+            configMenu.Register(
+                mod: this.ModManifest,
+                reset: () => this.Config = new ModConfig(),
+                save: () => this.Helper.WriteConfig(this.Config)
+            );
+
+            // Add config options
+            configMenu.AddNumberOption(
+                mod: this.ModManifest,
+                name: () => "Horizontal offset from right",
+                tooltip: () => "Sets how far the energy text is offset from the right side of the screen",
+                getValue: () => (int)this.Config.EnergyTextOffset.X,
+                setValue: value =>
+                {
+                    this.Config.EnergyTextOffset =
+                        this.Config.EnergyTextOffset with { X = value };
+                },
+                min: 0,
+                max: Game1.graphics.GraphicsDevice.Viewport.GetTitleSafeArea().Right);
+
+            configMenu.AddNumberOption(
+                mod: this.ModManifest,
+                name: () => "Vertical offset from bottom",
+                tooltip: () => "Sets how far the energy text is offset from the bottom side of the screen",
+                getValue: () => (int)this.Config.EnergyTextOffset.Y,
+                setValue: value =>
+                {
+                    this.Config.EnergyTextOffset =
+                        this.Config.EnergyTextOffset with { Y = value };
+                },
+                min: 0,
+                max: Game1.graphics.GraphicsDevice.Viewport.GetTitleSafeArea().Bottom);
         }
     }
 }
